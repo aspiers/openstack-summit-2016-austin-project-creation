0ea16adc77b8d331b37040ee33251a98a669b90e
Pranav Salunke <dguitarbite@gmail.com>
2016-04-22 09:45:39 +0200

    Adds a small python script to view the ppt.
    Using flask as a lightweight webserver launched on the fly configured to
    read and run the index.html. Please make sure that the submodules are
    initialized and updated before running it :).

---
+++ view.py
@@ -0,0 +1,25 @@
+from flask import Flask, send_from_directory
+import os
+
+
+DEBUG = False
+PORT = 55555
+PATH = os.path.abspath('.')
+print PATH
+
+
+def display_ppt(PATH):
+    app = Flask(__name__, static_url_path='')
+    html_path = PATH
+
+    @app.route('/austin-talk/<path:path>')
+    def presentation(path):
+        return send_from_directory(html_path, path)
+
+    app.run(host='127.0.0.1', port=PORT, debug=DEBUG)
+
+
+if __name__ == "__main__":
+
+    display_ppt(PATH)
+    print("Your ppt is here: localhost:55555/austin-talk/index.html")
